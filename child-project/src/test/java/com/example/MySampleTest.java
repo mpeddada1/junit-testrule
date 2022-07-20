8f402b35f94092d26c50acc801d0ec14c6b175f9
Mridula Peddada <mpeddada@google.com>
2022-07-20 18:06:00 -0400

    sample app to demonstrate NPE with native maven plugin 0.9.12 and later

---
+++ child-project/src/test/java/com/example/MySampleTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static com.google.common.truth.Truth.assertThat;
+import org.junit.Test;
+
+public class MySampleTest {
+
+    @Test
+    public void testNativeImage(){
+        assertThat("hi").isEqualTo("hi");
+    }
+}
